fb479c510643f9fee0a7de7b72ea16545939f1fd
wordsend <63223239+linkseguro@users.noreply.github.com>
2025-03-20 19:32:53 -0300

    Add files via upload

---
+++ cdn/shop/t/4/assets/custom.js
@@ -0,0 +1,2 @@
+function exibir(){$("#campo-cep").val().length>7?($(".frete-resultado").hide(),setTimeout(function(){$(".frete-resultado").show()},500)):alert("Insira um cep valido")}
+//# sourceMappingURL=/cdn/shop/t/4/assets/custom.js.map?v=167431315182516192201722964645
